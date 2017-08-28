9aa4e9ccac5052c1b58962929665ff7852592fb3
David Wilkie <dwilkie@gmail.com>
2017-08-28 18:13:48 +0700

    Remove loop. Only run sync once

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,23 +3,8 @@
 set -e
 
 if [ "$1" = 'sync' ]; then
-  : ${SLEEP_BETWEEN_SYNCS:=1}
   : ${LOCAL_DIR:=/data}
-
-  cleanup ()
-  {
-    kill -s SIGTERM $!
-    exit 0
-  }
-
-  trap cleanup SIGINT SIGTERM
-
-  while [ 1 ]
-  do
-    aws s3 sync $LOCAL_DIR $S3_PATH --sse
-    sleep $SLEEP_BETWEEN_SYNCS &
-    wait $!
-  done
+  exec aws s3 sync $LOCAL_DIR $S3_PATH --sse
+else
+  exec "$@"
 fi
-
-exec "$@"
